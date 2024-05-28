1eb5e2c070c048de127fb75a922b72c03b31469a
ledna A <ledna.ankenbauer@yahoo.com>
2024-05-28 22:17:33 +0000

    Create model and ensure it's in admin

--- foods_and_delicacies/models.py
+++ foods_and_delicacies/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Food(models.Model):
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    rating = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    
+    def __str__(self):
+        return f'Food: {self.name} {self.price}'
+    
+    
